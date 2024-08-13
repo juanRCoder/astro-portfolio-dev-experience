a68ff5aebc1043e44dcf043aee8487fc95a50882
juanRCoder <guillecarck@gmail.com>
2024-08-13 12:04:22 -0500

    feat: nuevo formato

---
+++ src/components/Redes.tsx
@@ -0,0 +1,18 @@
+import { FaGithub, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
+import { GrDocumentUser } from "react-icons/gr";
+import CV from '../images/JuanRamirez_CV.pdf';
+import '../styles/Redes.css';
+
+export default function Redes() {
+  return (
+    <div className="flex gap-5 relative text-xl text-slate-200 px-5">
+      <a className="name-red" target="_BLANK" href="https://github.com/juanRCoder" aria-label="Github"><FaGithub className="hover:scale-110 text-5xl" /></a>
+      <a className="name-red1" target="_BLANK" href="https://www.linkedin.com/in/juan-ramirez-490b84271/" aria-label="LinkedIn"><FaLinkedinIn className="hover:scale-110 text-5xl" /></a>
+      <a className="name-red2" target="_BLANK" href="https://wa.me/+51956402456" aria-label="WhatsApp"><FaWhatsapp className="hover:scale-110 text-5xl" /></a>
+      <a className="name-red3" href={CV} aria-label="Descargar CV" download="JuanRamirez_CV.pdf">
+        <GrDocumentUser className="hover:scale-110 text-5xl" />
+      </a>
+
+    </div>
+  )
+}
---
+++ src/utils/dataProjects.ts
@@ -0,0 +1,39 @@
+import GithubProfile from "../images/github-profile.png";
+import calculoFinanciero from "../images/calculo-financiero.png";
+import Cvinter from '../images/cvinter.png';
+
+export const project = [
+    {
+      id: 1,
+      img: GithubProfile,
+      name: "Github Profile",
+      description:
+        "Buscador de perfiles de github, en el cual puedes encontrar la informacion del usuario mediante el username en github.",
+      technologys: ["Vue", "TailwindCSS"],
+      github:
+        "https://github.com/juanRCoder/proyectos-VUE/tree/main/src/github-profile",
+      website: "https://vue-projects-r8hq.vercel.app/github-profile",
+    },
+    {
+      id: 2,
+      img: calculoFinanciero,
+      name: "Calculo Financiero",
+      description:
+        "Pagina web con finalidad de mostrar ingresos, gastos y ahorro economicos de manera grafica y expresiva.",
+      technologys: ["Angular", "TailwindCSS"],
+      github:
+        "https://github.com/juanRCoder/proyectos-ANGULAR/tree/main/src/app/calculo-financiero",
+      website: "https://angular-mini-projects.vercel.app/calculo-financiero",
+    },
+    {
+      id: 2,
+      img: Cvinter,
+      name: "CVinter App",
+      description:
+        "Pagina web en la cual puedes generar tu propio Curriculum tomando como plantilla 5 modelos muy atraticvos para las empresas.",
+      technologys: ["React", "TailwindCSS"],
+      github:
+        "https://github.com/juanRCoder/i002-igrowker-cvinter-front",
+      website: "https://i002-cvinter-front-phi.vercel.app/",
+    },
+  ];
