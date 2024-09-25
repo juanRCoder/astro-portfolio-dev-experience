6159f2d80e8a57b68071bb37dce6eddbac05b4c0
juanRCoder <guillecarck@gmail.com>
2024-09-25 15:45:42 -0500

    update: actualizacion de curriculum

--- src/components/Redes.tsx
+++ src/components/Redes.tsx
@@ -1,6 +1,6 @@
 import { FaGithub, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
 import { GrDocumentUser } from "react-icons/gr";
-import CV from '../images/JuanRamirez_CV.pdf';
+import CV from '../images/CV_DEVELOPER.pdf';
 import '../styles/Redes.css';
 
 export default function Redes() {
@@ -12,7 +12,7 @@ export default function Redes() {
           <a className="name-red" target="_BLANK" href="https://github.com/juanRCoder" aria-label="Github"><FaGithub className="hover:scale-110 text-5xl" /></a>
           <a className="name-red1" target="_BLANK" href="https://www.linkedin.com/in/juan-ramirez-490b84271/" aria-label="LinkedIn"><FaLinkedinIn className="hover:scale-110 text-5xl" /></a>
           <a className="name-red2" target="_BLANK" href="https://wa.me/+51956402456" aria-label="WhatsApp"><FaWhatsapp className="hover:scale-110 text-5xl" /></a>
-          <a className="name-red3" href={CV} aria-label="Descargar CV" download="JuanRamirez_CV.pdf">
+          <a className="name-red3" href={CV} aria-label="Descargar CV" download="CV_DEVELOPER.pdf">
             <GrDocumentUser className="hover:scale-110 text-5xl" />
           </a>
         </div>
